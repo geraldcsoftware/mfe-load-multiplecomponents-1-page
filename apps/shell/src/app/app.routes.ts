533938010d08d8b0bd2c31031bcb5e5b2796f4bd
Gerald Chifanzwa <gchifanzwa@zss.co.zw>
2023-04-28 14:26:34 +0200

    shell app

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Route } from "@angular/router";
+import {
+  loadRemoteEntry,
+  loadRemoteModule,
+} from '@angular-architects/module-federation';
+import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    outlet: 'head',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Component',
+      }).then((m) => m.AppComponent),
+  },
+  {
+    path: '',
+    outlet: 'content',
+    loadComponent: () => loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './Component',
+      }).then((m) => m.AppComponent),
+  },
+];
--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,11 +1,2 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from "@angular/router";
-import { appRoutes } from "./app/app.routes";
-import { AppComponent } from "./app/app.component";
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
-}).catch((err) => console.error(err));
+import('./bootstrap')
+	.catch(err => console.error(err));
